8e0e7d69e83fae05ed152d43c8fc9d884ce73579
Priyanka Pungaonkar <priyanka.pungaonkar95@gmail.com>
2025-06-02 18:01:59 +0530

    Add Assignment1 files and restructure project

---
+++ Assignment1/task_1.py
@@ -0,0 +1,17 @@
+#Takes two numbers as input from the user.
+num1 = int(input('Enter a first number: '))
+num2 = int(input('Enter a second number: '))
+
+#Performs the basic mathematical operations on these two numbers:
+# Addition, Subtraction, Multiplication, Division
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Displays the results of each operation on the screen.
+print('Addition: ', addition)
+print('Subtraction: ', subtraction)
+print('Multiplication: ', multiplication)
+print('Division: ', division)
+
